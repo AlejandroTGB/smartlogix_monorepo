d19a875c75bfa82f4c9e401042b92aeb884efe6a
Alejandro Torres <alej.torresg@duocuc.cl>
2026-05-08 02:57:18 -0400

    feat: agregar schemas para el registro de usuarios

--- microservicios/auth_service/schemas/auth_schema.py
+++ microservicios/auth_service/schemas/auth_schema.py
@@ -1,11 +1,30 @@
 from pydantic import BaseModel, EmailStr
 
+#============================
+# Entrada
+#============================
+
 class LoginRequest(BaseModel):
     email: EmailStr
     password: str
+
+class RegisterRequest(BaseModel):
+    email: EmailStr
+    password: str
+    nombre:str
+
+#============================
+# Salida
+#============================
 
 class LoginResponse(BaseModel):
     id: int
     nombre: str
     rol: str
     token: str
+
+class RegisterResponse(BaseModel):
+    id: int
+    email: EmailStr
+    nombre: str
+    rol: str
