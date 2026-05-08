f09cdfa7ed0a9d2fec24afacf8cf98832e845509
Alejandro Torres <alej.torresg@duocuc.cl>
2026-05-08 00:21:02 -0400

    refactor: delegar validacion de login a la capa service

--- microservicios/auth_service/routers/auth_router.py
+++ microservicios/auth_service/routers/auth_router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from schemas.auth_schema import LoginRequest, LoginResponse
+from services.auth_service import AuthService
 
 router = APIRouter(
     prefix="/api/v1/auth",
@@ -8,12 +9,12 @@ router = APIRouter(
 
 @router.post("/login", response_model=LoginResponse)
 async def login(credenciales: LoginRequest):
-    if credenciales.email == "admin@smartlogix.cl" and credenciales.password == "123":
-        return {
-            "id": 1,
-            "nombre": "Admin",
-            "rol": "admin",
-            "token":"test-jwt-valido"
-        }
-    else:
-        raise HTTPException(status_code=401, detail= "Credenciales incorrectas")
+    #datos al service
+    usuario_autenticado = AuthService.autenticar_usuario(credenciales)
+
+    #router decide que responder segun respuesta del service
+    if not usuario_autenticado:
+        raise HTTPException(status_code=401, detail="Credenciales incorrectas")
+    
+    #si sale bien, devolvemos el paquete
+    return usuario_autenticado
