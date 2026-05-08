70989276125abd920accd8965bf710e2aec5ff1e
Alejandro Torres <alej.torresg@duocuc.cl>
2026-05-08 03:50:45 -0400

    feat: agregar rutas de registro y login con inyección de dependencias

--- microservicios/auth_service/routers/auth_router.py
+++ microservicios/auth_service/routers/auth_router.py
@@ -1,20 +1,45 @@
-from fastapi import APIRouter, HTTPException
-from schemas.auth_schema import LoginRequest, LoginResponse
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy.orm import Session
+from schemas.auth_schema import LoginRequest, LoginResponse, RegisterRequest, UsuarioResponse
 from services.auth_service import AuthService
+from database import get_db
 
 router = APIRouter(
     prefix="/api/v1/auth",
     tags=["Autenticacion"]
 )
 
-@router.post("/login", response_model=LoginResponse)
-async def login(credenciales: LoginRequest):
-    #datos al service
-    usuario_autenticado = AuthService.autenticar_usuario(credenciales)
 
-    #router decide que responder segun respuesta del service
+#============================
+# Ruta de registro
+#============================
+@router.post("/registro", response_model=UsuarioResponse, status_code=201)
+async def registrar(
+    datos: RegisterRequest,
+    db: Session = Depends(get_db) #----> la inyeccion
+):  
+    #Pasamos datos y la conexión al service
+    nuevo_usuario = AuthService.registrar_usuario(db,datos)
+    
+    #Si el service devueve none es que ya estaba registrado ese corrreo
+    if not nuevo_usuario:
+        raise HTTPException(status_code=400, detail="El correo ya está registrado en el sistema.")
+    
+    #si sale bien, devolvemos el usuario
+    return nuevo_usuario
+
+#============================
+# Ruta de login
+#============================
+@router.post("/login", response_model=LoginResponse)
+async def login(
+    credenciales: LoginRequest,
+    db: Session = Depends(get_db)
+):
+    #pasamos conexion y datos al service
+    usuario_autenticado = AuthService.autenticar_usuario(db, credenciales)
+
     if not usuario_autenticado:
         raise HTTPException(status_code=401, detail="Credenciales incorrectas")
     
-    #si sale bien, devolvemos el paquete
     return usuario_autenticado
