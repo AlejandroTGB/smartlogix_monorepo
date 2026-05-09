a6f3fca00e7013dfa362c09a6cb4a7b812b3938f
Alejandro Torres <alej.torresg@duocuc.cl>
2026-05-09 18:33:20 -0400

    feat: agregar función de verificación de token JWT y esquema de seguridad HTTPBearer

--- microservicios/auth_service/core/security.py
+++ microservicios/auth_service/core/security.py
@@ -3,6 +3,10 @@ import jwt
 from datetime import datetime, timedelta, timezone
 from dotenv import load_dotenv
 
+from fastapi import HTTPException, Security
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from jwt.exceptions import PyJWTError
+
 #esto busca el .env y lo carga en la memoria temporal de python
 load_dotenv()
 
@@ -10,6 +14,7 @@ SECRET_KEY = os.getenv("JWT_SECRET_KEY", "clave_por_defecto_insegura")
 ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("JWT_ACCESS_TOKEN_EXPIRE_MINUTES", 60))
 
+#crear token
 def crear_token_acceso(datos: dict):
     to_encode = datos.copy()
     expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
@@ -19,3 +24,22 @@ def crear_token_acceso(datos: dict):
 
     return encoded_jwt
 
+#verifica token
+
+esquema_seguridad = HTTPBearer()
+
+def verificar_token(credenciales: HTTPAuthorizationCredentials = Security(esquema_seguridad)):
+    #sacamos el texto del token
+    token = credenciales.credentials
+
+    try:
+        #intentomas decodificar el token con la llave secreta
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        #si sale bien devolvemos datos del usuario
+        return payload
+    
+    except PyJWTError:
+        #si el token expiro, la firma no coincide o es texto basura tiramos error
+        raise HTTPException(status_code=401, detail="Token de acceso inválido o expirado. Acceso denegado.",
+                            headers={"WWW-Authenticate": "Bearer"},
+                            )
