ca740807b8aa5cb8f1f241f6cffb0e0aa13c645b
Alejandro Torres <alej.torresg@duocuc.cl>
2026-05-11 15:13:37 -0400

    feat: añadidos types de autenticacion

---
+++ frontend/src/types/auth.ts
@@ -0,0 +1,27 @@
+export interface User {
+  id: number;
+  email: string;
+  nombre: string;
+  rol: string;
+}
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+export interface RegisterRequest {
+  email: string;
+  password: string;
+  nombre: string;
+}
+export interface AuthResponse {
+  id: number;
+  nombre: string;
+  rol: string;
+  token: string;
+}
+export interface RegisterResponse {
+  id: number;
+  email: string;
+  nombre: string;
+  rol: string;
+}
